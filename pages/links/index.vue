88964903ff1ee95e31cee298c3f6f2b64f8168df
rm.abao <rm.abales.abao@gmail.com>
2026-03-02 00:22:43 +0800

    support page query param - pagination

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import axios from 'axios'; 
 import { TailwindPagination } from 'laravel-vue-pagination';
+import type { PaginatedResponse, Link } from "@/types";
 
-const data = ref([]);
-const page = ref(1);
+const data = ref<PaginatedResponse<Link> | null>(null);
+const page = ref(useRoute().query.page || 1);
+
 
 await getLinks();
-let links = computed(() => data.value.data);
+let links = computed(() => data.value?.data);
 
 
 
 watch(page, async ()=>{
   getLinks();
+  useRouter().push({
+    query: {
+      page: page.value
+    }
+  })
 })
 
 async function getLinks() {
@@ -83,7 +90,7 @@ definePageMeta({
           </tr>
         </tbody>
       </table>
-      <TailwindPagination :data="data" @pagination-change-page="$event"></TailwindPagination>
+      <TailwindPagination :data="data" @pagination-change-page="page = $event"></TailwindPagination>
       <div class="mt-5 flex justify-center"></div>
     </div>
 
